f0150f3b9ad9d89f9fb90d928cd8608250dad8ec
Auhood <>
2021-02-01 11:30:58 +0400

    sample

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:example_app/sences/Home/Homepage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "My App ", home: HomePage());
+  }
+
+  }
---
+++ lib/sences/Home/Homepage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+
+
+  @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+}
+class HomeState extends State<HomePage>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar:AppBar (title:Text ("Home page")),);
+  }
+}
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
